9473f358779176a0d00a1cb629eaca39176fc783
Khrove <joshualjohnson33@gmail.com>
2021-01-18 11:40:46 -0500

    Finished pig game, begin behind the scenes

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -12,35 +12,79 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-// Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+const switchPlayer = function() {
+    // Player rolled a 1, change player and reset currentScore
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    // If activeplayer strictly equals 0 then activeplayer becomes 1, else activeplayer becomes o
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    // Change active player background to display on the correct player
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+}
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
+let scores, currentScore, activePlayer, playing;
+
+const init = function() {
+    // Starting conditions
+    scores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
+
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    document.getElementById('current--0').textContent = 0;
+    document.getElementById('current--1').textContent = 0;
+
+    diceEl.classList.add('hidden');
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+}
+
+// Begin the game on page load
+init();
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function() {
-   // Generate a random dice roll between 1 - 6
-   const dice = Math.trunc(Math.random() * 6) + 1;
+    if(playing) {
+        // Generate a random dice roll between 1 - 6
+        const dice = Math.trunc(Math.random() * 6) + 1;
 
-   // Display the dice
-    diceEl.classList.remove('hidden');
-    diceEl.src = `dice-${dice}.png`;
+        // Display the dice
+        diceEl.classList.remove('hidden');
+        diceEl.src = `dice-${dice}.png`;
 
-   // Check for a rolled 1: if true, switch to next player and clear score
-    if(dice !== 1) {
-        currentScore += dice;
-        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
-    } else {
-        // Player rolled a 1, change player and reset currentScore
-        document.getElementById(`current--${activePlayer}`).textContent = 0;
-        // If activeplayer strictly equals 0 then activeplayer becomes 1, else activeplayer becomes o
-        activePlayer = activePlayer === 0 ? 1 : 0;
-        // Change active player background to display on the correct player
-        player0El.classList.toggle('player--active');
-        player1El.classList.toggle('player--active');
+        // Check for a rolled 1: if true, switch to next player and clear score
+        if(dice !== 1) {
+            currentScore += dice;
+            document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+        } else {
+            switchPlayer();
+        }
     }
 });
+
+btnHold.addEventListener('click', function() {
+    if(playing) {
+        //  1. Add current score to active player's score
+        scores[activePlayer] += currentScore;
+        document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer]
+
+        // 2. Check if player's score is >= 100
+        if(scores[activePlayer] >= 20) {
+            // If yes, then finish the game
+            playing = false;
+            diceEl.classList.add('hidden');
+            document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+            document.querySelector(`.player--${activePlayer}`).classList.remove('player-active');
+        } else {
+            // If no then switch to next player
+            switchPlayer();
+        }
+    }
+});
+
+btnNew.addEventListener('click', init);
